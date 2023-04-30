28dc634aa8ac6ac6e725725540b9d7d68a39325c
Peterson dos Santos <peterson.opensource@hotmail.com>
2023-04-29 21:47:33 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+const textoTeste = "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](http://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)."
+
+function extraiLinks(texto) {
+    const regex = /\[([^[]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
+    const capturas = [...texto.matchAll(regex)]
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados.length !== 0 ? resultados : chalk.red('não ha links no arquivo!'); 
+}   
+
+extraiLinks(textoTeste)
+
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'houve um problema ao abrir o arquivo!'));
+}
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding = 'utf-8'
+        const texto = await fs.promises.readFile(caminhoDoArquivo,
+             encoding)
+        return extraiLinks(texto);
+    }catch(erro) {
+        trataErro(erro)
+    }
+}
+
+ export default pegaArquivo;
